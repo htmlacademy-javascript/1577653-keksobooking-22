3696c35ff30b912d8f8c0aed6d6a40fa1aedd8b5
Keks <keks@htmlacademy.ru>
2021-02-22 19:38:50 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/main.js
@@ -0,0 +1,12 @@
+import {CARDS_COUNT, makeCards} from './make-cards.js';
+import {createCardLayout} from './create-card-layout.js';
+import './map.js'
+
+const mockObjects = makeCards(CARDS_COUNT);
+// const mapCanvas = document.querySelector('.map__canvas');
+// const card = createCardLayout(mockObjects[0]);
+// mapCanvas.appendChild(card);
+
+
+
+
